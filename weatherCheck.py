c47739e1f65a018dcde77cf3ad7897bfbd62f46c
petertbates <88071272+petertbates@users.noreply.github.com>
2021-08-01 08:38:40 -0500

    working draft file 8.3

--- weatherCheck.py
+++ weatherCheck.py
@@ -1,46 +1,85 @@
 #Peter Bates
-#CIS 245 Assignment 7.1 - Virtual Garage
+#CIS 245 Project
+#openweathermap api
+
+import requests, json
+
+#openweathermap info:
+baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
+apiKey = "fa93214eb4f60ad4decef8dc78b38ebf"
 
 def main():
 	#This is the main function, takes input and gets weather
-	menuSelection = ''
 	print("Welcome, let's check the weather")
+	menuSelection = ''
 	menuSelection = mainMenu(menuSelection)
 	while menuSelection != 'exit':
 		menuSelection = inputCheck(menuSelection)
-		print(menuSelection)
-		print(type(menuSelection))
-		getWeather(menuSelection)
+		while menuSelection == '':
+			menuSelection = mainMenu(menuSelection)
+			menuSelection = inputCheck(menuSelection)
+		if menuSelection != '' and menuSelection != 'exit':
+			weather = getWeather(menuSelection)
+			displayWeather(weather)
+		
 		menuSelection = mainMenu(menuSelection)
 		
 def mainMenu(menuSelect):
 	#Initial selection to see if things want to exit
 	if menuSelect != 'exit':
-		menuSelect = input('Please enter a zip code or city. To exit, type "exit": ')
+		menuSelect = input('\nPlease enter a zip code or city. To exit, type "exit": ')
 	return menuSelect
 
 def inputCheck(menuChoice):
 	#we want to see if their input is blank, and continue reprompt until filled
 	while menuChoice == '':
-		menuChoice = input('\nPlease enter a zip code, city, or type "exit": ')
+		menuChoice = input('\nEnter a zip code, city, or "exit": ')
 
 	#first check for call to exit before anything else
 	if(menuChoice.lower() == 'exit'):
 		menuChoice = 'exit'
+		return menuChoice
 	elif(menuChoice != ''):
 		try:
 			int(menuChoice)
 		except ValueError:
-			print("MenuChoice to get back to citycheck")
-			return(menuChoice)
-
+			url = baseUrl + "appid=" + apiKey + "&q=" + menuChoice
+			return url
 		else:
-			print("checking zipcode")
-			return zipCode
+			if len(menuChoice) != 5:
+				print("Please enter a 5-digit zipcode.")
+				url = ''
+			else:
+				url = baseUrl + "appid=" + apiKey + "&zip=" + menuChoice
+			return url
 
-def getWeather(location):
-	#This will call using location to get weather information to pass back
-	pass
+def getWeather(url):
+	response = requests.get(url)
+	return response.json()
+
+def displayWeather(weather):
+	if(weather["cod"] == '404'):
+		print ("Sorry, " + weather["message"] + ".")
+	else:
+		location = weather["name"]
+		describeWeather = weather["weather"]
+		description = describeWeather[0]["description"]
+		temperatures = weather["main"]
+	
+		temp = temperatures["temp"]
+		feelTemp = temperatures["feels_like"]
+		#Temp conversion (K − 273.15) × 9/5 + 32 = °F.
+		temp = round((((temp - 273.15) * 9) / 5) + 32)
+		feelTemp = round((((feelTemp - 273.15) * 9) / 5) + 32)
+
+		currentWeather = "Currently " + location + " has " + description + ". "
+		currentWeather += "\nThe temperature is " + str(temp) + "°F, and it feels like " + str(feelTemp) + "°F."
+
+		print(currentWeather)
 
 main()
 #just call the main
+
+#Credit:
+#https://openweathermap.org/current#list
+#https://nineplanets.org/kids/temperature-conversion/
